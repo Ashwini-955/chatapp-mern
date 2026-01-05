ddeed9724e42790fcc10334dfeb93cc1db2a0a9e
Ashwini955 <ashwinirm052@gmail.com>
2026-01-05 23:12:29 +0530

    current chatters feature added

--- backend/routControllers/userHandleController.js
+++ backend/routControllers/userHandleController.js
@@ -1,7 +1,10 @@
+import Conversation from "../Models/conversationModels.js";
+import User from "../Models/userModels.js";
+
 export const getUserBySearch=async(req,res)=>{
     try {
         const search =req.query.search || '';
-        const currentUserId=req.user._conditions._id;
+        const currentUserId = req.user._id;
         const user =await User.find({
             $and:[{
                 $or:[
@@ -23,3 +26,45 @@ export const getUserBySearch=async(req,res)=>{
         console.log(error);
     }
 }
+
+export const getCurrentChatters = async (req, res) => {
+  try {
+    const currentUserId = req.user._id;
+
+    const currentChatters = await Conversation.find({
+      participants: currentUserId
+    }).sort({ updatedAt: -1 });
+
+    if (!currentChatters || currentChatters.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    // get all other participant IDs
+    const participantIds = currentChatters.reduce((ids, conversation) => {
+      const others = conversation.participants.filter(
+        id => id.toString() !== currentUserId.toString()
+      );
+      return [...ids, ...others];
+    }, []);
+
+    // remove duplicates
+    const uniqueParticipantIds = [
+      ...new Set(participantIds.map(id => id.toString()))
+    ];
+
+    const users = await User.find({
+      _id: { $in: uniqueParticipantIds }
+    })
+      .select("-password")
+      .select("email username profilepic");
+
+    return res.status(200).json(users);
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import isLogin from '../middlewares/isLogin.js'
-import { getUserBySearch } from "../routControllers/userHandleController.js";
+import { getUserBySearch,getCurrentChatters } from "../routControllers/userHandleController.js";
 
 const router =express.Router()
 
-router.get('/search',isLogin,getUserBySearch)
+router.get('/search',isLogin,getUserBySearch);
+router.get('/currentchatters',isLogin,getCurrentChatters);
 export default router
